38036ade72e09427075e56d4779d85563626b21e
Steffany <kielen@gmail.com>
2018-05-23 11:36:24 -0700

    updated html layout with nested grids and updated css to match

---
+++ main.js
@@ -0,0 +1,14 @@
+function loadDocument(){
+    setClickHandlers();
+}
+function setClickHandlers(){
+    function columnClickHandler(){
+    };
+    function powerupButtonClickHandler(){
+    };
+    function resetButtonClickHandler(){
+    };
+    columnClickHandler();
+    powerupButtonClickHandler();
+    resetButtonClickHandler();
+}
